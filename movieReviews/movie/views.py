0fba2b3a7cb409b3d16c4e955fe39c9e27d862b2
mehmettan <mhmettan@gmail.com>
2022-10-14 16:45:56 +0300

    bootstrap was added to the website.

--- movieReviews/movie/views.py
+++ movieReviews/movie/views.py
@@ -1,12 +1,10 @@
-from curses.ascii import HT
-import imp
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Welcome to Home page<\h1>')
+    return render(request, 'home.html', {'name':'Greg Lim'})
 
 
 def about(request):
