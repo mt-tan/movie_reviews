d13abda1a9fd359100321450b0a9bd0becd382f1
mehmettan <mhmettan@gmail.com>
2022-10-14 14:43:21 +0300

    welcome and about html pages were generated.

--- movieReviews/movie/views.py
+++ movieReviews/movie/views.py
@@ -1,3 +1,13 @@
+from curses.ascii import HT
+import imp
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('<h1>Welcome to Home page<\h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>Welcome to About page<\h1>')
