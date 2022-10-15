efe364d5f46509655fd038a256e6a5f89fd0eb06
mehmettan <mhmettan@gmail.com>
2022-10-15 17:01:54 +0300

    Visual aspects of the website was enhanced using card components.

--- movieReviews/movie/views.py
+++ movieReviews/movie/views.py
@@ -1,12 +1,14 @@
 import email
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 
 def home(request):
     searchterm = request.GET.get("searchMovie")
-    return render(request, 'home.html', {"searchterm":searchterm})
+    movies = Movie.objects.all() 
+    return render(request, 'home.html', {"searchterm":searchterm, 'movies': movies})
 
 
 def about(request):
--- movieReviews/movieReviews/settings.py
+++ movieReviews/movieReviews/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for movieReviews project.
 
@@ -122,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
