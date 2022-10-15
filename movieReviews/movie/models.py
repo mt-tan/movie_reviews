d28b7d19c1e4956c33ee2072e43569a3a378bda9
mehmettan <mhmettan@gmail.com>
2022-10-15 11:54:21 +0300

    migration files for database connection were created.

--- movieReviews/movie/models.py
+++ movieReviews/movie/models.py
@@ -1,3 +1,11 @@
+from operator import mod
+from turtle import title
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='movie/images/')
+    url = models.URLField(blank=True)
