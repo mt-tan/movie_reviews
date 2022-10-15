fb6ccff3539273d969e58a955d5ad665b6d539ef
mehmettan <mhmettan@gmail.com>
2022-10-15 11:07:16 +0300

    html pages were updated.

--- movieReviews/movie/views.py
+++ movieReviews/movie/views.py
@@ -1,11 +1,18 @@
+import email
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'name':'Greg Lim'})
+    searchterm = request.GET.get("searchMovie")
+    return render(request, 'home.html', {"searchterm":searchterm})
 
 
 def about(request):
     return HttpResponse('<h1>Welcome to About page<\h1>')
+
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email':email})
