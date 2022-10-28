a838326b56fc63d2c32b06c981682269fe5cb754
mehmettan <mhmettan@gmail.com>
2022-10-28 16:10:31 +0300

    movie detail page was added to the application.

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,9 @@
 import email
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from .models import Movie
+
 
 # Create your views here.
 
@@ -21,3 +23,9 @@ def about(request):
 def signup(request):
     email = request.GET.get('email')
     return render(request, 'signup.html', {'email':email})
+
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie,pk=movie_id)
+    return render(request, 'detail.html', 
+    {'movie':movie})
